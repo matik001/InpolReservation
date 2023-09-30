c29a792e675232baca1e75e9390d0812b08326a9
Mateusz Kisiel <mateusz.kisiel.mk@gmail.com>
2023-09-30 09:01:56 +0200

    Add project files.

---
+++ InpolReservation/InpolConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InpolReservation
+{
+    internal class InpolConfig
+    {
+        public int CheckIntervalMs { get; set; }
+        public string TicketType { get; set; }
+
+    }
+}
